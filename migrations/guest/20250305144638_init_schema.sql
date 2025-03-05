4e23cbd1aaac74ee89cd95aba4dae00fda1a09bf
Jansen Choi Kai Xuan <140486215+choijans@users.noreply.github.com>
2025-03-05 23:31:35 +0800

    Add files via upload

---
+++ migrations/guest/20250305144638_init_schema.sql
@@ -0,0 +1,27 @@
+-- +goose Up
+-- SQL statements for migrating up
+
+CREATE TABLE guests (
+    guest_id SERIAL PRIMARY KEY,
+    full_name VARCHAR(100) NOT NULL,
+    birthdate DATE,
+    contact_info JSONB,  -- Stores phone, email, etc.
+    address TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE guest_preferences (
+    preference_id SERIAL PRIMARY KEY,
+    guest_id INTEGER NOT NULL REFERENCES guests(guest_id),
+    preference_type VARCHAR(50),
+    preference_value TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- +goose Down
+-- SQL statements for migrating down
+
+DROP TABLE IF EXISTS guest_preferences;
+DROP TABLE IF EXISTS guests;
